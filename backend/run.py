9ba1e05c9d8e41836a9d693fcb42fa51fdf8b3a4
luyisi2016 <luyisi2016@gmail.com>
2025-05-23 20:27:24 +0100

    create render file and move requirements file to backend

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,8 @@
+import os
 from app import create_app
 
-# Abstract the app object
 app = create_app()
 
-if __name__ == "__main__":
-    app.run (debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render sets this env variable
+    app.run(host='0.0.0.0', port=port)
